09323508f505350be85bfb997315fe8cf693bf3f
Xiongwei Lin <80614539+xiongweilin@users.noreply.github.com>
2026-09-18 19:17:29 +0800

    fix: align alembic imports with project package grouping

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,9 @@ from __future__ import annotations
 import os
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from autonomous_development.adapters.postgres.schema import metadata
 
 config = context.config
